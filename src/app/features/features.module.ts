dcaa9d98f9b2b5965cfea15366e77628cf588379
Franco_Osores <franco_osores@hotmail.com>
2022-08-13 07:25:06 -0300

    Feat: FormField component generated

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { FeaturesRoutingModule } from './features-routing.module';
 import { CustomButtonComponent } from '../components/custom-button/custom-button.component';
 import { HomeComponent } from './views/home/home.component';
+import { FormFieldComponent } from '../components/form-field/form-field.component';
 
 
 @NgModule({
   declarations: [
     CustomButtonComponent,
-    HomeComponent
+    HomeComponent,
+    FormFieldComponent
   ],
   imports: [
     CommonModule,
